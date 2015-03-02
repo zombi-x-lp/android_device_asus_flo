5cc5030d7d7055dec714911ea953ea98be25b575
Hardslog <hardslog@gmail.com>
2015-03-02 09:47:45 -0500

    Lets make the kernel Selinux Permissive
    We don't like Selinux anyway.  Especially since xposed needs a permissive kernel to work properly :)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,7 +29,7 @@ TARGET_NO_BOOTLOADER := false
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 # BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 maxcpus=2
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 BOARD_USES_ALSA_AUDIO:= true
