381ee33f2a534fb6b08a8580647606b6b81c8e4e
Joshua <jl52741@gmail.com>
2015-04-12 03:21:08 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,6 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-add_lunch_combo aosp_flo-userdebug
-add_lunch_combo omni_flo-userdebug
